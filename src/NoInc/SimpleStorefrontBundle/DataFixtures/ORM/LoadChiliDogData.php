1a1b56f9a8bdba4d116689c3927f20f9d695fa7a
johnreubank <johnreubank@gmail.com>
2016-05-07 18:51:17 -0400

    added recipe for chili dogs & ingredients

--- src/NoInc/SimpleStorefrontBundle/DataFixtures/ORM/LoadChiliDogData.php
+++ src/NoInc/SimpleStorefrontBundle/DataFixtures/ORM/LoadChiliDogData.php
@@ -10,7 +10,7 @@ use NoInc\SimpleStorefrontBundle\Entity\Ingredient;
 use NoInc\SimpleStorefrontBundle\Entity\Recipe;
 use NoInc\SimpleStorefrontBundle\Entity\RecipeIngredient;
 
-class LoadLemonadeData extends AbstractFixture implements OrderedFixtureInterface
+class LoadChiliDogData extends AbstractFixture implements OrderedFixtureInterface
 {
     public function load(ObjectManager $manager)
     {
@@ -18,30 +18,30 @@ class LoadLemonadeData extends AbstractFixture implements OrderedFixtureInterfac
         foreach ( $this->ingredientArray() as $ingredientData )
         {
             $ingredient = new Ingredient();
-            
+
             $ingredient->setName($ingredientData["name"]);
             $ingredient->setPrice($ingredientData["price"]);
             $ingredient->setMeasure($ingredientData["measure"]);
             $ingredient->setStock(100);
-    
+
             $manager->persist($ingredient);
-        
+
             $ingredients[$ingredient->getName()] = $ingredient;
         }
         $manager->flush();
-        
+
         $recipeData = $this->recipeArray();
-        
+
         $recipe = new Recipe();
         $recipe->setName($recipeData["name"]);
         $recipe->setPrice($recipeData["price"]);
         $manager->persist($recipe);
         $manager->flush();
-        
+
         foreach( $recipeData["ingredients"] as $recipeIngredientData )
         {
             $recipeIngredient = new RecipeIngredient();
-            
+
             $recipeIngredient->setIngredient($ingredients[$recipeIngredientData["name"]]);
             $recipeIngredient->setRecipe($recipe);
             $recipeIngredient->setQuantity($recipeIngredientData["quantity"]);
@@ -49,52 +49,63 @@ class LoadLemonadeData extends AbstractFixture implements OrderedFixtureInterfac
         }
         $manager->flush();
     }
-    
+
     public function ingredientArray()
     {
         return [
             [
-                "name" => "Lemon",
-                "price" => 0.10,
-                "measure" => "Juice"
+                "name" => "Hot Dog",
+                "price" => 0.50,
+                "measure" => "Frank"
             ],
             [
-                "name" => "Sugar",
-                "price" => 0.10,
+                "name" => "Hot Dog Roll",
+                "price" => 0.25,
+                "measure" => "Bun"
+            ],
+            [
+                "name" => "Chili",
+                "price" => 0.20,
                 "measure" => "Cup"
             ],
             [
-                "name" => "Water",
-                "price" => 0.00,
+                "name" => "Cheese",
+                "price" => 0.10,
                 "measure" => "Cup"
             ]
+
         ];
     }
-    
+    //comment
     public function recipeArray()
     {
         return [
-            "name" => "Lemonade",
-            "price" => 1.00,
+            "name" => "Chili Dog",
+            "price" => 2.00,
             "ingredients" => [
                 [
-                    "name" => "Lemon",
-                    "quantity" => 2
+                    "name" => "Hot Dog",
+                    "quantity" => 1
                 ],
                 [
-                    "name" => "Sugar",
+                    "name" => "Hot Dog Roll",
+                    "quantity" => 1
+                ],
+                [
+                    "name" => "Chili",
                     "quantity" => 0.5
                 ],
-                [
-                    "name" => "Water",
-                    "quantity" => 4
-                ],
+				[
+					"name" => "Cheese",
+					"quantity" => 0.5
+				],
             ]
         ];
     }
-    
+
     public function getOrder()
     {
-        return 2;
+        return 3;
     }
 }
+
